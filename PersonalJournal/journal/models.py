4b62ad00bb490bb9e874a46bf520a3d759c686bf
34codezak <zacharyz4qke@gmail.com>
2025-03-27 08:17:55 +0300

    Add basic user profile feature for uploading a profile picture

--- PersonalJournal/journal/models.py
+++ PersonalJournal/journal/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# User model extension
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics', help_text='Upload Your Profile Picture')
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 class JournalEntry(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +24,10 @@ class JournalEntry(models.Model):
 
     def __str__(self):
         return self.title
+    
+# Signal to automatically create a profile when a user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        instance.profile.save()
